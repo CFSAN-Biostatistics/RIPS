4195ebb88dccf73b24c364ba2fba7dcedf7f4415
TimPM14 <144161995+TimPM14@users.noreply.github.com>
2023-12-21 13:31:52 -0500

    Add files via upload

---
+++ install.R
@@ -0,0 +1,22 @@
+#!/usr/bin/Rscript
+
+#Modified from genomeSidekick:https://github.com/dchapski/genomeSidekick
+# This script should get you the dependencies required for RIPS
+# Might want to run by hand so you do not overwrite custom installations
+# More info:
+# https://statsandr.com/blog/an-efficient-way-to-install-and-load-r-packages/
+
+# Define a function to negate the %in% function in R
+`%notin%` <- Negate(`%in%`)
+
+# List all dependencies
+dependencies <- c("rvest", "httr", "Rcurl", "dplyr", "shiny", "data.table", "DT", "purr", "xml2", "ggplot2", "lubridate", "plotly", "stringr", "tidyr", "readr", "shinydashboard", "shinyWidgets", "rintrojs"
+)
+
+# Check which ones are not met by using %notin%
+not.met <- dependencies %notin% rownames(installed.packages())
+
+# Install unmet dependencies
+if(any(not.met)) {
+  install.packages(dependencies[not.met])
+}
